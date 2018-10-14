b468650e8a8bffa9511b37cd49da385b2e926e41
manzor99 <manzor99@gmail.com>
2018-10-14 19:41:29 -0400

    Update Application.java

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,7 +1,7 @@
 package com.springboot;
 
 import java.util.Arrays;
-
+import java.awt.Toolkit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
